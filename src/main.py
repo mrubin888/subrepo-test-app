004ec2e1e8781003fbc379b058b4148f7948e45f
Matt Rubin <mrubin888@gmail.com>
2021-03-06 22:32:21 -0800

    Basic setup

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+from src.some_other import SOME_OTHER_CONSTANT
+
+
 def do_something():
     print("Hello world!")
+    print(f"Some other constant: {SOME_OTHER_CONSTANT}")
+
 
 if __name__ == "__main__":
     do_something()
